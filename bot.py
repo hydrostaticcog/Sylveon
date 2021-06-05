15ac795257417479ec181fe3e72582d4c74eaa00
valkyrie_pilot <69158910+randomairborne@users.noreply.github.com>
2021-06-04 18:51:40 -0600

    make bot blush if someone tries to hug Glaceon

--- bot.py
+++ bot.py
@@ -152,17 +152,21 @@ async def hug(ctx, members: commands.Greedy[discord.Member] = None, *, reason="a
         mentions = " ".join(mentions)
     elif ctx.message.mention_everyone:
         mentions = "@everyone"
+        reason = reason.replace("@everyone","")
     else:
         mentions = " :D"
-    await ctx.send(f"{mentions}, {ctx.author.mention} gave you a hug, {reason}")
-    hugs = ["https://media.tenor.com/images/50c2f13c590fdb27c087d6a6736218e0/tenor.gif",
-            "https://media.discordapp.net/attachments/731763704005394523/829133807008743444/image0.gif",
-            "https://media1.tenor.com/images/969f0f462e4b7350da543f0231ba94cb/tenor.gif",
-            "https://media1.tenor.com/images/b7492c8996b25e613a2ab58a5d801924/tenor.gif?itemid=14227401",
-            "https://media1.tenor.com/images/24ac13447f9409d41c1aecb923aedf81/tenor.gif?itemid=5026057",
-            "https://media1.tenor.com/images/f720d87668fa1e65b3294eb30fc4ac36/tenor.gif?itemid=19092449",
-            "https://cdn.discordapp.com/attachments/611750243964420128/611750305897512974/image0.gif"]
-    await ctx.send(random.choice(hugs))
+    for person in members:
+        if person.id == 808149899182342145:
+            await ctx.send("https://tenor.com/view/anime-blush-girl-gif-19459906")
+        await ctx.send(f"{mentions}, {ctx.author.mention} gave you a hug, {reason}")
+        hugs = ["https://media.tenor.com/images/50c2f13c590fdb27c087d6a6736218e0/tenor.gif",
+                "https://media.discordapp.net/attachments/731763704005394523/829133807008743444/image0.gif",
+                "https://media1.tenor.com/images/969f0f462e4b7350da543f0231ba94cb/tenor.gif",
+                "https://media1.tenor.com/images/b7492c8996b25e613a2ab58a5d801924/tenor.gif?itemid=14227401",
+                "https://media1.tenor.com/images/24ac13447f9409d41c1aecb923aedf81/tenor.gif?itemid=5026057",
+                "https://media1.tenor.com/images/f720d87668fa1e65b3294eb30fc4ac36/tenor.gif?itemid=19092449",
+                "https://cdn.discordapp.com/attachments/611750243964420128/611750305897512974/image0.gif"]
+        await ctx.send(random.choice(hugs))
 
 
 @sylveon.event
