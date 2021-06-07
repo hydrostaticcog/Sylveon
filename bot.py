1a8ca0251172c9bbc8df0946fd2c728000bfa902
BuildTools <81295020+Dog-gamer124@users.noreply.github.com>
2021-06-07 19:15:59 +0530

    fixed me being dumb

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ class Help(commands.MinimalHelpCommand):
         await channel.send(embed=embed)
 
 
-with open(path / 'token.txt', 'r') as file:
+with open(path / 'system/token.txt', 'r') as file:
     TOKEN = file.read()
 intents = discord.Intents().all()
 sylveon = commands.Bot(command_prefix=prefixgetter, case_insensitive=True, intents=intents,
