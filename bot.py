26a4cb5ebdb6402b91fafae8602cb7256ed5e165
randomairborne <gxhutchinson@gmail.com>
2021-07-12 01:17:24 -0600

    Format leave message better

--- bot.py
+++ bot.py
@@ -306,7 +306,7 @@ async def leave(ctx, guild_id: typing.Optional[int] = None):
     if guild_id is None:
         guild_id = ctx.guild.id
     guild = sylveon.get_guild(guild_id)
-    await ctx.reply("Leaving guild " + guild.name)
+    await ctx.reply("Leaving guild `" + guild.name + "`!")
     await guild.leave()
 
 
